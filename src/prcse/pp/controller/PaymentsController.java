3e15ebf6a0212a33b7295ef8a9c6b55b24a52b8c
Alex Sims <alexmk92@live.co.uk>
2014-02-13 22:28:11 +0000

    Added payment view, added Oracles scene stackpane loading framework and modified to suit the scope of the project, added spinner and various other UI elements to aid HCI, built custom list views.

---
+++ src/prcse/pp/controller/PaymentsController.java
@@ -0,0 +1,283 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License"). You
+ * may not use this file except in compliance with the License. You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */ 
+
+package prcse.pp.controller;
+
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import prcse.pp.model.XCell;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author Angie
+ */
+public class PaymentsController implements Initializable, ControlledScreen {
+    // Refernce FXML Variables
+    @FXML // fx:id="Draggable"
+    private BorderPane draggable;
+    @FXML // fx:id="closeBtn"
+    private Button closeBtn;
+    @FXML // fx:id="maximBtn"
+    private Button maximBtn;
+    @FXML // fx:id="minimBtn"
+    private Button minimBtn;
+    @FXML // fx:id="nav1"
+    private AnchorPane nav1;
+    @FXML // fx:id="nav2"
+    private AnchorPane nav2;
+    @FXML // fx:id="nav3"
+    private AnchorPane nav3;
+    @FXML // fx:id="nav4"
+    private AnchorPane nav4;
+    @FXML // fx:id="nav5"
+    private AnchorPane nav5;
+    @FXML // fx:id="nav6"
+    private AnchorPane nav6;
+    @FXML // fx:id="accent1"
+    private Rectangle accent1;
+    @FXML // fx:id="accent2"
+    private Rectangle accent2;
+    @FXML // fx:id="accent3"
+    private Rectangle accent3;
+    @FXML // fx:id="accent4"
+    private Rectangle accent4;
+    @FXML // fx:id="accent5"
+    private Rectangle accent5;
+    @FXML // fx:id="accent6"
+    private Rectangle accent6;
+    @FXML // fx:id="title"
+    private Label title;
+    @FXML // fx:id="lstPayments"
+    private ListView lstPayments;
+    @FXML // fx:id="btnSearchPayments"
+    private Button btnSearchPayments;
+    @FXML // fx:id="spinner"
+    private ImageView spinner;
+
+    // Set variables to allow for draggable window.
+    private double xOffset = 0;
+    private double yOffset = 0;
+    ScreensController myController;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resources)
+    {
+        // Set the display graphic for title
+        Effect glow = new Glow(0.3);
+        title.setEffect(glow);
+
+        // Set the draggable component
+        draggable.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        draggable.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                draggable.getScene().getWindow().setX(event.getScreenX() - xOffset);
+                draggable.getScene().getWindow().setY(event.getScreenY() - yOffset);
+            }
+        });
+
+        nav1.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent1.setStyle("visibility: visible");
+            }
+        });
+        nav1.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent1.setStyle("visibility: hidden");
+            }
+        });
+        nav2.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent2.setStyle("visibility: visible");
+            }
+        });
+        nav2.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent2.setStyle("visibility: hidden");
+            }
+        });
+        nav3.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent3.setStyle("visibility: visible");
+            }
+        });
+        nav3.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent3.setStyle("visibility: hidden");
+            }
+        });
+        nav5.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent5.setStyle("visibility: visible");
+            }
+        });
+        nav5.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent5.setStyle("visibility: hidden");
+            }
+        });
+        nav6.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent6.setStyle("visibility: visible");
+            }
+        });
+        nav6.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                accent6.setStyle("visibility: hidden");
+            }
+        });
+
+        // Search payment button handler
+        btnSearchPayments.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String user_details = null;
+
+                if(user_details != null)
+                {
+                    btnSearchPayments.getStyleClass().add("searching");
+                    btnSearchPayments.setText("Searching...");
+                    spinner.getStyleClass().remove("hidden");
+                } else {
+                    btnSearchPayments.getStyleClass().remove("searching");
+                    btnSearchPayments.setText("Search Payments");
+                    spinner.getStyleClass().add("hidden");
+                }
+
+            }
+        });
+
+        // Set test data, this will populate with Payee name, amount and time
+        ObservableList<String> values = FXCollections.observableArrayList("Alex Sims", "Jamie Shepherd", "Adam Stevenson", "Thomas Knowles", "Jason Dee");
+        lstPayments.setItems(values);
+        lstPayments.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+            @Override
+            public ListCell<String> call(ListView<String> param) {
+                return new XCell();
+            }
+        });
+
+        // Utility controls
+        closeBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                Platform.exit();
+            }
+        });
+
+
+    }
+
+    // Set the parent of the new screen
+    public void setScreenParent(ScreensController screenParent){
+        myController = screenParent;
+    }
+
+    // Navigation Control
+    @FXML
+    private void goToDashboard(ActionEvent event){
+        myController.setScreen(ScreensFramework.screen1ID);
+    }
+    @FXML
+    private void goToUsers(ActionEvent event){
+        myController.setScreen(ScreensFramework.screen2ID);
+    }
+    @FXML
+    private void goToProperties(ActionEvent event){
+        myController.setScreen(ScreensFramework.screen3ID);
+    }
+    @FXML
+    private void goToPayments(ActionEvent event){
+       myController.setScreen(ScreensFramework.screen4ID);
+    }
+    @FXML
+    private void goToMessages(ActionEvent event){
+       myController.setScreen(ScreensFramework.screen5ID);
+    }
+    @FXML
+    private void goToSettings(ActionEvent event){
+        myController.setScreen(ScreensFramework.screen6ID);
+    }
+}
+
+
+
